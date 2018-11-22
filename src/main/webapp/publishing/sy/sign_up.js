f73160d77a739c996ae4da62e1139a7e3720a484
ksy <woro104@gmail.com>
2018-11-22 15:11:48 +0900

    [2018.11.22 김세영] 로그인, 회원가입, Mypage UI 75%정도 완료

---
+++ src/main/webapp/publishing/sy/sign_up.js
@@ -0,0 +1,35 @@
+function LoadImg(value) {
+    if(value.files && value.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $("#LoadImg").attr('src', e.target.result);
+        }
+        reader.readAsDataURL(value.files[0]);
+    }
+}
+// var sel_file;
+// $(document).ready(function() {
+//     $("#input_img").on("change", handleImgFileSelect);
+// });
+
+// function handleImgFileSelect(e) {
+//     var files = e.target.files;
+//     var filesArr = Array.prototype.slice.call(files);
+
+//     filesArr.forEach(function(f) {
+//         if(!f.type.match("image.*")) {
+//             alert("확장자는 이미지 확장자만 가능합니다.");
+//             return;
+//         }
+
+//         sel_file = f;
+
+//         var reader = new FileReader();
+//         reader.onload = function(e) {
+//             $("#img").attr("src", e.target.result);
+//         }
+//         reader.readAsDataURL(f);
+//     });
+        
+// }
+  
